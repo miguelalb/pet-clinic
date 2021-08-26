c088dd0a827bf16e2a5b708d784100604092d96e
Miguel Acevedo <44342378+miguelalb@users.noreply.github.com>
2021-08-26 15:38:45 -0400

    Initial Commit

---
+++ src/test/java/com/miguelacevedo/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguelacevedo.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
